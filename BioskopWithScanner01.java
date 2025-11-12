88a69b485705fc9db7b4313d43991dec19d848ad
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2025-11-12 12:38:57 +0700

    Percobaan 2 No.2

---
+++ BioskopWithScanner01.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class BioskopWithScanner01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int baris, kolom, menu;
+        String nama, next;
+        String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.println("\n===== MENU BIOSKOP =====");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            menu = sc.nextInt();
+            sc.nextLine();
+
+            if (menu == 1) {
+                while (true) {
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris: ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
+
+                    if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                        System.out.println("Posisi tidak valid! Baris (1–4), Kolom (1–2).");
+                    } else if (penonton[baris - 1][kolom - 1] != null) {
+                        System.out.println("Kursi sudah ditempati!");
+                    } else {
+                        penonton[baris - 1][kolom - 1] = nama;
+                    }
+
+                    System.out.print("Input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+
+            } 
+            else if (menu == 2) {
+                System.out.println("\n===== DAFTAR PENONTON =====");
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            System.out.print("** Kosong **\t");
+                        } 
+                        else {
+                            System.out.print(penonton[i][j] + "\t");
+                        }
+                    }
+                    System.out.println();
+                }
+
+            } 
+            else if (menu == 3) {
+                System.out.println("Program selesai.");
+                break;
+
+            } 
+            else {
+                System.out.println("Menu tidak valid, Pilih 1-3.");
+            }
+        }
+        sc.close();
+    }
+}
